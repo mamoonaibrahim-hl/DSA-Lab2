be5fc3a9cb6c4d806d96b69e3a0c01cc1b5704d9
Mamoona Ibrahim <mamoonaibrahim10c@gmail.com>
2026-09-16 11:37:03 +0500

    task 1 added

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Memory in bytes of int: "<<sizeof(int)<<endl;
+    cout<<"Memory in bytes of long: "<<sizeof(long)<<endl;
+    cout<<"Memory in bytes of float: "<<sizeof(float)<<endl;
+    cout<<"Memory in bytes of double: "<<sizeof(double)<<endl;
+    cout<<"Memory in bytes of char: "<<sizeof(char)<<endl;
+    return 0;
+}
